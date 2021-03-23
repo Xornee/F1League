cc6111d2a6cf862ec60add98cc42d225b5ce4e35
Szymon <szymon.mytych@protonmail.com>
2021-03-23 11:21:31 +0100

    1.0.5 commit

--- src/container/heroSection.js
+++ src/container/heroSection.js
@@ -5,7 +5,7 @@ export function HeroContainer() {
         <HeroSection image={'/images/main.png'}>
             <HeroSection.TextWrapper>
                 <HeroSection.TextBig>Rekrutacja do 3 sezonu nadal trwa!</HeroSection.TextBig>
-                <HeroSection.Button href={'https://docs.google.com/forms/d/e/1FAIpQLSff6wwmqOMGugRgV59iivgfc1gFEsHRkXF4gG9s8bsi6w06wQ/viewform'} target={'_blank'}>Zarejestruj Sie</HeroSection.Button>
+                <HeroSection.Button href={'https://docs.google.com/forms/d/e/1FAIpQLSff6wwmqOMGugRgV59iivgfc1gFEsHRkXF4gG9s8bsi6w06wQ/viewform'} target={'_blank'}>Rejestracja</HeroSection.Button>
                 <HeroSection.Text>Przejdz rekrutacje i scigaj sie u nas!</HeroSection.Text>
             </HeroSection.TextWrapper>
         </HeroSection>
