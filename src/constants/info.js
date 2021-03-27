0c79605df25493ff88a563dca430b159b86ebdfe
Szymon <szymon.mytych@protonmail.com>
2021-03-27 12:08:43 +0100

    1.0.8 - 01110011 01101001 01100101 01101101 01100001 01101110 01101111

--- src/constants/info.js
+++ src/constants/info.js
@@ -8,6 +8,7 @@ const data = [
       postedOn: " 23.03.2020 ",
       author: " Kamil Drapik ",
       blogImage: "/images/post1.png",
+      TextImage: "/images/post2.png",
       blogText: ['🟠GP Bahrajnu już za nami, tym samym Frevky okazał się królem pustyni, plasując się na 1 miejscu w klasyfikacji wyścigowej oraz klasyfikacji generalnej. Za nim na podium znaleźli się kolejno Foxello oraz Sonix.',
         '🔵Zmagania kierowców były bardzo wyrównane, co przekładało się na niesamowicie zaciętą walkę - od początku do końca stawki. Na tor dwukrotnie wyjechał samochód bezpieczeństwa, który pokrzyżował strategię kilku zawodników. Kilku kierowców stawki zaliczyło DNF.',
         '⚫Dziękujemy wszystkim kierowcom, do zobaczenia w przyszły czwartek na węgierskim Hungaroringu.Szerokości!'
@@ -21,7 +22,8 @@ const data = [
       link: "Examplelink2",
       postedOn: " 27.03.2020 ",
       author: " Kamil Drapik ",
-      blogImage: "/images/post2.png",
+      blogImage: "/images/post2.jpg",
+      TextImage: "/images/post2.jpg",
       blogText: ['Na pierwszym miejscu w klasyfikacji wyścigowej znalazł się Sonix, za nim kolejno Bartosh oraz Ropuch. \n' +
       'Zmagania kierowców od samego początku rywalizacji trzymały w napięciu. Zaczynając od świetnych startów, przechodząc przez ciekawe wyprzedzenia i ogólne przetasowanie stawki, kończąc na wielu niespodziewanych wykluczeniach i wypadkach. Wszystko to spowodowało, że czwartkowy wyścig był na prawdę ciekawym widowiskiem. ',
       '🔵Zmagania kierowców były bardzo wyrównane, co przekładało się na niesamowicie zaciętą walkę - od początku do końca stawki. Na tor dwukrotnie wyjechał samochód bezpieczeństwa, który pokrzyżował strategię kilku zawodników. Kilku kierowców stawki zaliczyło DNF.',
